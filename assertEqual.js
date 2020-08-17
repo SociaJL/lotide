fd4995db448596eaa5cd6ca00b571af57ade1dad
SociaJL <socialconsequences@gmail.com>
2020-08-17 23:21:59 +0000

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("Assertion Passed");
+  } else if (actual !== expected) {
+    console.log("Assertion Failed: actual !== expected");
+  }
+  
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
