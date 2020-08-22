8468c6fb6d4fb83c4501e083a6e3cab348b9f14e
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-21 18:46:52 -0600

    middle elements of array

---
+++ middle.js
@@ -0,0 +1,19 @@
+let theMiddle = []; // empty array to store results in. 
+
+const middle = function (array) {
+  if (array.length <= 2) {      // returns [] on arrays <= 2. 
+    return theMiddle;
+
+  } else if (array.length % 2 !== 0) {   // returns middle element on arrays that are odd. 
+    return (Math.round(array.length / 2));
+
+  } else if (array.length % 2 === 0) {  // returns middle 2 elemtnts in an even array. 
+    return [(Math.round(array.length / 2)), (array.length / 2 + 1)];
+
+  };
+};
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
