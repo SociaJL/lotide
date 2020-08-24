812dc9c253c67fa58a15abdad4bf60423505d79e
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 12:26:41 -0600

    updated

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 const tail = function (array) {
   let newArray = array.slice(1)
   return newArray;
 }
 
-assertEqual(tail([5, 6, 7])[0], [6, 7][0]); 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"])[0], ["Lighthouse", "Labs"][0]);
-
+module.exports = tail; 
