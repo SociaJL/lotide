23c50511ddf73a75869ed404d526877d01a13999
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 14:46:49 -0600

    mocha head

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,13 @@
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
 
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
