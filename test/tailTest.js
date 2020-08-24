6a198246a1bbadb17d5ddc1d1dbd34b9d7d9bad1
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 15:20:34 -0600

    mocha tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,13 @@
-const assertEqual = require('../assertEqual.js');
+
 const tail = require('../tail.js');
+const assert = require('chai').assert;
 
+describe("#tail", () => {
+  it("returns '5' for [5, 6, 7 && '6' for [6, 7]", () => {
+    assert.strictEqual(tail([5, 6, 7])[0], [6, 7][0]); 
+  });
+});
 
-assertEqual(tail([5, 6, 7])[0], [6, 7][0]); 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"])[0], ["Lighthouse", "Labs"][0]);
-
+it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs'] && 'Lighthouse' for ['Lighthouse', 'Labs']", () => {
+  assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], ["Lighthouse", "Labs"][0]);
+});
