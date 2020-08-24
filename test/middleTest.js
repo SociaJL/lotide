ad8802cdb515a9babb4a58e5a1aa9bef1519510f
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 14:20:58 -0600

    middleTest

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const middle = require('../middle.js'); 
+
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+
+
