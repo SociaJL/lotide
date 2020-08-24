1e6d0c11659d7760e86a0355793d0bb5cf0af6d5
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 11:57:18 -0600

    refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,12 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("Assertion Passed");
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("Assertion Failed: actual !== expected");
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
   
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
+module.exports = assertEqual;
 
