96874efcb3da467a50269cd28277b14c15e07489
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-23 20:12:17 -0600

    countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,7 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
 
-const firstNames = [     
+const firstNames = [
   "Karl",
   "Salima",
   "Agouhanna",
@@ -11,40 +13,20 @@ const firstNames = [
   "Joe"
 ];
 
-const assertEqual = function (actual, expected) {
 
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    }
-
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
-    }
     console.log(item);
   }
-  return results;
 
-}
+  return results;
+};
+
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+console.log(result1["Jason"], 1);
+console.log(result1["Karima"], undefined);
+console.log(result1["Fang"], 2);
+console.log(result1["Agouhanna"], undefined);
