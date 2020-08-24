d0e1b6c9e95f61a81a1479626181b2f6abd9a711
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-08-24 15:51:32 -0600

    middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,16 @@
 const middle = require('../middle.js'); 
+const assert = require('chai').assert;
 
+describe("#middle", () => {
+    it("returns [] if the array length is <= 2 ", () => {
+    assert(middle([1, 2]));
+   });
+});
 
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+it("returns the middle element on array lengths that are odd", () => {
+   assert(middle([1, 2, 3, 4, 5]));
+});
 
-
+it("returns the middle two elements when the array length is even", () => {
+  assert(middle([1, 2, 3, 4, 5, 6]));
+});
