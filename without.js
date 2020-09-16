174722394a41d3a8a058b5414740b146f1d1f9fd
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-15 20:14:05 -0600

    updated

--- without.js
+++ without.js
@@ -1,33 +1,17 @@
 // Implement without which will return a subset of a given array, removing unwanted elements.
+// return the first array with any elements removed that are present in the second array. 
+
 
 /*
+Ternary example
 const sentence = 'The quick brown fox jumps over the lazy dog.';
-
 const word = 'fox';
-
 console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
-// expected output: "The word "fox" is in the sentence"
+
+expected output: "The word "fox" is in the sentence"
 */
 
 /*
-loop1:
-    for (var i in set1) {
-loop2:
-        for (var j in set2) {
-loop3:
-            for (var k in set3) {
-                break loop2;  // breaks out of loop3 and loop2
-            }
-        }
-    }
-*/
-
-const without = function(array, without) {
-  let result = [];
-    for (let i = 0; i < array.length; i++) {
-      if (!without.includes(array[i])) result.push(array[i]);
-    }
-  /*
     for (let i = 0; i < array.length; i++) {
       let match = false;
       for (let j = 0; j < without.length; j++) {
@@ -35,9 +19,20 @@ const without = function(array, without) {
       }
       if (match === false) result.push(array[i]);
     }
-    */
+*/
+
+
+// function takes in 2 arrays
+const without = function(array, without) {
+// an empty array accumilator
+  let result = [];
+// loops through every element in the first array
+    for (let i = 0; i < array.length; i++) {
+// If the first array index does not contain a value in an index of the second array, then push that index value into the empty array accumilator.
+      if (!without.includes(array[i])) result.push(array[i]); // <----- need further review to understand why the [without] array isnt mentioned. 
+    }
+// return the current value of the array accumilator
   return result;
 }
-
+// test case
 console.log(without([1, 2, 3], [5, 6, 1, 55, 12, 23])) // => [2, 3]
-// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
