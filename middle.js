bbdf6e51a40a6eee5a17135812dcc2c1ab20311d
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-16 11:26:27 -0600

    updated

--- middle.js
+++ middle.js
@@ -1,16 +1,33 @@
-let theMiddle = []; // empty array to store results in. 
 
+// given an array, return an array with only the middle elements 
+// for arrays with one or two elements, return an empty array 
+// for arrays with odd mumber of elements, return an array containing the middle element. 
+// for arrays with an even mumber of elements, return an array containing the the two elements in the middle
+
+
+//need an array accumilator in order to return an empty array
+let emptyArray = []; 
+// middle function to check the array for length and do something depending on what length is discovered. 
 const middle = function (array) {
-  if (array.length <= 2) {      // returns [] on arrays <= 2. 
-    return theMiddle;
-
-  } else if (array.length % 2 !== 0) {   // returns middle element on arrays that are odd. 
+// checks if the length of the array as 2 or less elements if so, it returns the empty array accumilator
+  if (array.length <= 2) {      
+    return emptyArray;
+// checks if the array is an odd length by using the modulus operator. if remainder does not equal 0, then it must be odd. 
+  } else if (array.length % 2 !== 0) {  
+// if the array is odd, divide its length by two and rounds down to the next index. 
     return (Math.round(array.length / 2));
-
-  } else if (array.length % 2 === 0) {  // returns middle 2 elemtnts in an even array. 
+// checks if the array is even by using the modulus opperator. if remainder equals 0, than its even. 
+  } else if (array.length % 2 === 0) {  
+// returns the index position of the two middle elements. 
     return [(Math.round(array.length / 2)), (array.length / 2 + 1)];
 
   };
 };
 
 module.exports = middle; 
+ 
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
