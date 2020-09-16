4c63acf65c3431d98440a119e06c47093e34244e
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-15 19:59:17 -0600

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,23 @@
 
+// Function to take in 2 arrays and compare them. 
 const assertArraysEqual = function(actual, expected) {
   // return true/false comparing two arrays and their values
+  // matches is set to true
   let matches = true;
-  if (actual.length !== expected.length) { // break if length not equal
+  // compares the length of both arrays, if not equal length, then they cannot be equal value.
+  if (actual.length !== expected.length) { 
+  // this will set matches to false if run because matches is a locally scoped variable.
     matches = false;
   }
+  // loops through each element in array
   for (let i = 0; i < actual.length; i++) {
+  // if the element in the arrays dont match, fase will be returned
     if (actual[i] !== expected[i]) matches = false;
   }
+  // if the arrays length and value match
   if (matches) {
     console.log(`Does Match: ${actual} === ${expected}`);
+  // if the arrays length and value dont match
   } else {
     console.log(`Doesn\'t Match: ${actual} !== ${expected}`);
   }
